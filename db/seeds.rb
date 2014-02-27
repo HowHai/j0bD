3a12b35ce2e4ae0f70ac95641fdaa6c5c5fab75b
Hai Nguyen <hmn91733@gmail.com>
2014-02-26 18:13:03 -0800

    Add employee stackoverflow's data to their show page.
    [completes #66561380]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Employee.destroy_all
+StackOverflowAccount.destroy_all
+StackTag.destroy_all
+GithubAccount.destroy_all
+GithubRepo.destroy_all
