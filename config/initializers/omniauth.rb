0c8195bc579ba9ed94cad0d16753f61678e2ce55
ScottBigbee <scottbigbee@hotmail.com>
2014-02-28 09:34:34 -0800

    before feb 28 morning pull

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,3 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # Github
   provider :github, '5bc3f4b827fdc3094fe5', '97015c95bf9309499a984fced3cec140206178ad', scope: 'user, public_repo, repo'
 end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :linkedin, "consumer_key", "consumer_secret"
+end
