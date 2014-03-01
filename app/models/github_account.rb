003f0920a16b6ba913a0e27a66d9a5892494dd9c
ScottBigbee <scottbigbee@hotmail.com>
2014-03-01 13:14:31 -0800

    Test that github_accounts model belongs_to employee model. [completes #66722614]

--- app/models/github_account.rb
+++ app/models/github_account.rb
@@ -2,6 +2,10 @@ class GithubAccount < ActiveRecord::Base
   belongs_to :employee
   has_many :github_repos
 
+  validates :employee_id, :provider, :uid, :oauth_token, presence: true
+  validates_inclusion_of :provider, in:['github']
+
+  
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
