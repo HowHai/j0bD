c10d1fc607d67430c183b1daa81576597e4e1487
Hai Nguyen <hmn91733@gmail.com>
2014-03-03 19:53:54 -0800

    Add seed datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,22 @@ end
 15.times do
   Employee.create(name: random_name)
 end
+
+# Create different linkedin positions
+# Random titles
+def random_position_title
+  ['Web Designer', 'Front End Developer', 'Front End Engineer', 'JavaScript Developer', 'JavaScript Engineer', 'Visual Designer', 'UX Designer', 'Interaction Designer', 'Web Developer', 'Full Stack Developer', 'Web Programmer', 'Web Application Developer', 'Rails Developer', 'Back End Developer'].sample
+end
+
+# Create a random summary with random job related keywords added in
+def random_position_summary
+  random_skills = ['HTML', 'PHP', 'CSS3', 'CSS', 'JavaScript', 'jQuery', 'ajax', 'ruby', 'ruby on rails', 'coffeescript', 'pythong', 'c', 'viml', 'Go', 'Clojure', 'Prolog', 'haskell', 'perl', 'lua', 'scala']
+
+  random_summary = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, maxime, praesentium, rem ducimus iusto harum perferendis officiis numquam veritatis nobis accusamus laborum dolor nihil dolores cum ex beatae nostrum totam. "
+
+  # Add 3 random skills to summary
+  3.times do
+    random_summary += random_skills.sample + " "
+  end
+  random_summary
+end
