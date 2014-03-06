3aec85a54970736ab5d4f16536894619ef396ed0
ScottBigbee <scottbigbee@hotmail.com>
2014-03-05 16:28:03 -0800

    test for current_employee in EmployeesController

--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe EmployeesController do
+  let(:current_employee) { FactoryGirl.create :employee}
+  before :each do
+    controller.stub(:current_employee).and_return(current_employee) 
+  end
 
-  # describe "GET 'update'" do
-  #   it "returns http success" do
-  #     get 'update'
-  #     response.should be_success
-  #   end
-  # end
+  describe "GET 'show'" do
+    it "returns http success" do
+      Employee.stub(:find).with(current_employee.id).and_return("yourMama")
+      get 'show', {id: 18}
+      expect(assigns(:employee)).to eq "yourMama"
+      response.should be_success
+    end
+  end
 
   describe "GET 'new'" do
     it "returns http success" do
--- spec/controllers/linkedins_controller_spec.rb
+++ spec/controllers/linkedins_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe LinkedinsController do
-
+  describe "GET index" do
+    it "responds to success" do
+      get 'index'
+    end
+  end
 end
