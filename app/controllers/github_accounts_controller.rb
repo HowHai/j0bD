213f5c41957908a862219019e8dfa9ba45fdbd83
Hai Nguyen <hmn91733@gmail.com>
2014-03-07 23:06:01 -0800

    Fix github controller bugs

--- app/controllers/github_accounts_controller.rb
+++ app/controllers/github_accounts_controller.rb
@@ -7,16 +7,16 @@ class GithubAccountsController < ApplicationController
     github_user = GithubAccount.find_by_provider_and_uid(auth[:provider], auth[:uid])
 
     if github_user.nil?
-      @employee = @employee.create
+      @employee = Employee.create
       github_user = @employee.create_github(auth)
 
+      session[:user_id] = @employee.id
       # Create repositories
       allRepos = repos_languages_data(github_user.username)
       allRepos.each do |repo|
         merged_repo = repo.merge(github_account_id: github_user.id)
         GithubRepo.create(merged_repo)
       end
-      session[:user_id] = @employee.id
     else
       @employee = github_user.employee
       session[:user_id] = @employee.id
