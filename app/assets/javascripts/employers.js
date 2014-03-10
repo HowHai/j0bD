91543f98b01ccf6e80a84d3c2402b4ef00170567
James Calhoun <jmacalhoun@Jamess-MacBook-Pro.local>
2014-03-09 23:05:29 -0700

    commit to pull haimar8

--- app/assets/javascripts/employers.js
+++ app/assets/javascripts/employers.js
@@ -19,6 +19,21 @@ window.loadEmployeesPage = function() {
 
       // displays compiled template with things in a div called content
       $('#employees-display').html(template(data));
+
+      var data = [1,2,3,4];
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 200]);
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
+
+
     });
   });
 };
