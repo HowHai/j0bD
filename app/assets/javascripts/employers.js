57893e5886945404bf245b46d99299d95945d555
James Calhoun <jmacalhoun@Jamess-MacBook-Pro.local>
2014-03-10 04:12:35 -0700

    improve landing page

--- app/assets/javascripts/employers.js
+++ app/assets/javascripts/employers.js
@@ -23,20 +23,22 @@ window.loadEmployeesPage = function() {
       // D3 test
       // Get top 4 skills
       var firstStats = Math.floor(parseFloat($("#first_stats").text()) / 5000);
-      var secondStats = Math.floor(parseFloat($("#second_stats").text()) / 5000);
-      var thirdStats = Math.floor(parseFloat($("#third_stats").text()) / 5000);
+      var secondStats = Math.floor(parseFloat($("#second_stats").text()) / 13000);
+      var thirdStats = Math.floor(parseFloat($("#third_stats").text()) / 13000);
       var fourthStats = Math.floor(parseFloat($("#fourth_stats").text()) / 5000);
 
-      $("#first_stats").html(firstStats);
-      $("#second_stats").html(secondStats);
-      $("#third_stats").html(thirdStats);
-      $("#fourth_stats").html(fourthStats);
+      $("#first_stats").html('HTML');
+      $("#second_stats").html('Ruby');
+      $("#third_stats").html('Javascript');
+      $("#fourth_stats").html('PHP');
 
-      d3.selectAll("p").style("color", "white");
-      d3.selectAll("p").style("background-color", "black");
+      d3.selectAll("#first_stats").style("background-color","orange");
+      d3.selectAll("#second_stats").style("background-color","green");
+      d3.selectAll("#third_stats").style("background-color","blue");
+      d3.selectAll("#fourth_stats").style("background-color","lavender");
       function applyD3Selectors(){
       var barLength = [firstStats, secondStats, thirdStats, fourthStats];
-      d3.selectAll("p")
+      d3.selectAll(".prof-text")
           .data(barLength)
           .style("width", function(d) { return d + "px"; });
       }
--- app/views/employers/index.json.jbuilder
+++ app/views/employers/index.json.jbuilder
@@ -1,3 +1,3 @@
 json.employee do |json|
-  json.extract! @employee, :id, :name, :primary_stats
+  json.extract! @employee, :id, :name, :primary_stats, :github_account, :stack_overflow_account, :linked_in, :dribbble
 end
