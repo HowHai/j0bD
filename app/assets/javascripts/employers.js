09c9c35b4b32a2c76c7acfff9f0454f6ec3cdd24
James Calhoun <jmacalhoun@Jamess-MacBook-Pro.local>
2014-03-10 06:48:47 -0700

    Make save button run stats method on employee page

--- app/assets/javascripts/employers.js
+++ app/assets/javascripts/employers.js
@@ -20,42 +20,51 @@ window.loadEmployeesPage = function() {
       // displays compiled template with things in a div called content
       $('#employees-display').html(template(data));
 
-// OLD D3
-//       var data = [1,2,3,4];
-
-// var x = d3.scale.linear()
-//     .domain([0, d3.max(data)])
-//     .range([0, 200]);
-
-// d3.select(".chart")
-//   .selectAll("div")
-//     .data(data)
-//   .enter().append("div")
-//     .style("width", function(d) { return x(d) + "px"; })
-//     .text(function(d) { return d; });
-
-
       // D3 test
       // Get top 4 skills
-      var firstStats = Math.floor(parseFloat($("#first_stats").text()) / 5000);
-      var secondStats = Math.floor(parseFloat($("#second_stats").text()) / 13000);
-      var thirdStats = Math.floor(parseFloat($("#third_stats").text()) / 13000);
-      var fourthStats = Math.floor(parseFloat($("#fourth_stats").text()) / 5000);
 
-      $("#first_stats").html('HTML');
-      $("#second_stats").html('Ruby');
-      $("#third_stats").html('Javascript');
-      $("#fourth_stats").html('PHP');
 
-      d3.selectAll("#first_stats").style("background-color","orange");
-      d3.selectAll("#second_stats").style("background-color","green");
-      d3.selectAll("#third_stats").style("background-color","blue");
-      d3.selectAll("#fourth_stats").style("background-color","lavender");
+
+
+          // BORDER
+
+
+
+
+
       function applyD3Selectors(){
-      var barLength = [firstStats, secondStats, thirdStats, fourthStats];
-      d3.selectAll(".prof-text")
-          .data(barLength)
-          .style("width", function(d) { return d + "px"; });
+
+      var firstStats = Math.floor(parseFloat($("#first_stats").text()));
+      var secondStats = Math.floor(parseFloat($("#second_stats").text()));
+      var thirdStats = Math.floor(parseFloat($("#third_stats").text()));
+      var fourthStats = Math.floor(parseFloat($("#fourth_stats").text()));
+      
+      // $("#first_stats").html(firstStats);
+      // $("#second_stats").html(secondStats);
+      // $("#third_stats").html(thirdStats);
+      // $("#fourth_stats").html(fourthStats);
+
+      // d3.selectAll("#first_stats").style("background-color","orange");
+      // d3.selectAll("#second_stats").style("background-color","green");
+      // d3.selectAll("#third_stats").style("background-color","blue");
+      // d3.selectAll("#fourth_stats").style("background-color","lavender");
+
+      // d3.selectAll(".prof-text")
+      //     .data(barLength)
+      //     .style("width", function(d) { return d + "px"; });
+
+// DIVIDER
+      var data = [firstStats, secondStats, thirdStats, fourthStats];
+      var x = d3.scale.linear()
+          .domain([0, d3.max(data)])
+          .range([0, 200]);
+
+      d3.select(".text-container")
+        .selectAll("prof-text")
+          .data(data)
+        .enter().append(".prof-text")
+          .style("width", function(d) { return x(d) + "px"; })
+          .text(function(d) { return d; });
       }
 
       applyD3Selectors();
